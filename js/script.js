b4f89195e455a5f890ea90476cee3c40522061e3
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-03-12 12:24:02 +0530

    js files added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+alert("Hello !! Welcome to RoyalDine.Inn")
+
+// allow notification popup box 
+var x = confirm(" Allow Notification From RoyalDine.Inn ");
+    if(x == true){
+        confirm("Allow Notification From RoyalDine.Inn");
+    }
+
+// changing the image when clicked
+// function changeImage(){
+//     let img = document.getElementById('foodselect')
+//     img.src = 'hero'
+// }
